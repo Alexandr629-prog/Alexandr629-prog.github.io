77bb1cd95cb932a373a9c5afb5fae0493b0b092c
Alexandr629-prog <aaskaldovic@gmail.com>
2020-04-08 18:58:39 +0300

    corrected the task

--- Singolo/script.js
+++ Singolo/script.js
@@ -1,4 +1,50 @@
 
+window.onscroll = function showHeader() {
+  var header = document.querySelector('header');
+  if(window.pageYOffset > 130){
+      header.classList.add('header_fixed'); 
+      //document.querySelectorAll("body > header > ul > li.home.red").forEach(red=>red.classList.remove('red'));
+  } else{
+      header.classList.remove('header_fixed');
+  }
+  if(window.pageYOffset < 500){
+    document.querySelectorAll("body > header > ul > li.home").forEach(home=>home.classList.add('red'));
+    document.querySelectorAll("body > header > ul > li:nth-child(2) > span").forEach(option2=>option2.classList.remove('option2'));
+    document.querySelectorAll("body > header > ul > li:nth-child(3) > span").forEach(option2=>option2.classList.remove('option2'));
+    document.querySelectorAll("body > header > ul > li:nth-child(4)>span").forEach(option2=>option2.classList.remove('option2'));
+    document.querySelectorAll("body > header > ul > li:nth-child(5)>span").forEach(option2=>option2.classList.remove('option2'));
+  }
+  if(window.pageYOffset > 500 && window.pageYOffset < 1000){
+    document.querySelectorAll("body > header > ul > li.home.red").forEach(red=>red.classList.remove('red'));
+    document.querySelectorAll("body > header > ul > li:nth-child(3) > span").forEach(option2=>option2.classList.remove('option2'));
+    document.querySelectorAll("body > header > ul > li:nth-child(4)>span").forEach(option2=>option2.classList.remove('option2'));
+    document.querySelectorAll("body > header > ul > li:nth-child(5)>span").forEach(option2=>option2.classList.remove('option2'));
+    document.querySelectorAll("body > header > ul > li:nth-child(2) > span").forEach(option2=>option2.classList.add('option2'));
+  }
+  if(window.pageYOffset > 1000 && window.pageYOffset < 1900){
+    document.querySelectorAll("body > header > ul > li.home.red").forEach(red=>red.classList.remove('red'));
+    document.querySelectorAll("body > header > ul > li:nth-child(2) > span").forEach(option2=>option2.classList.remove('option2'));
+    document.querySelectorAll("body > header > ul > li:nth-child(4)>span").forEach(option2=>option2.classList.remove('option2'));
+    document.querySelectorAll("body > header > ul > li:nth-child(5)>span").forEach(option2=>option2.classList.remove('option2'));
+    document.querySelectorAll("body > header > ul > li:nth-child(3) > span").forEach(option2=>option2.classList.add('option2'));
+  }
+  if(window.pageYOffset > 1900 && window.pageYOffset < 2600)
+  {
+    document.querySelectorAll("body > header > ul > li.home.red").forEach(red=>red.classList.remove('red'));
+    document.querySelectorAll("body > header > ul > li:nth-child(2) > span").forEach(option2=>option2.classList.remove('option2'));
+    document.querySelectorAll("body > header > ul > li:nth-child(3) > span").forEach(option2=>option2.classList.remove('option2'));
+    document.querySelectorAll("body > header > ul > li:nth-child(5)>span").forEach(option2=>option2.classList.remove('option2'));
+    document.querySelectorAll("body > header > ul > li:nth-child(4)>span").forEach(option2=>option2.classList.add('option2'));
+  }
+  if(window.pageYOffset > 2600 && window.pageYOffset < 3000) 
+  {document.querySelectorAll("body > header > ul > li.home.red").forEach(red=>red.classList.remove('red'));
+  document.querySelectorAll("body > header > ul > li:nth-child(2) > span").forEach(option2=>option2.classList.remove('option2'));
+  document.querySelectorAll("body > header > ul > li:nth-child(3) > span").forEach(option2=>option2.classList.remove('option2'));
+  document.querySelectorAll("body > header > ul > li:nth-child(4)>span").forEach(option2=>option2.classList.remove('option2'));
+  document.querySelectorAll("body > header > ul > li:nth-child(5)>span").forEach(option2=>option2.classList.add('option2'));
+  }
+}
+
 document.querySelector("body > header > ul > li:nth-child(3) > span").addEventListener('click', ()=> {
   if(event.type==='click'){
     document.querySelector("body > div.Portfolio").scrollIntoView({ behavior: 'smooth' });
@@ -22,8 +68,7 @@ document.querySelector("body > header > ul > li:nth-child(5)>span").addEventList
 });  
 
 const handler1 = (event) => {
-
-  if(event.type==='click')
+  if(event.type==='click' )
   {
     document.querySelectorAll("body > header > ul > li.home.red").forEach(red=>red.classList.remove('red'));
     document.querySelectorAll("body > header > ul > li:nth-child(2) > span").forEach(option2=>option2.classList.remove('option2'));
@@ -38,19 +83,6 @@ const handler1 = (event) => {
   
   event.target.classList.add('option2');   
 }
-
-/*document.querySelector('body > header > ul > li:nth-child(2) > span').addEventListener('click', ()=> {
-  if(event.type==='click')
-  {
-  event.target.style.color='red';
-  document.querySelector('body > header > ul > li.home').style.color= 'white';
-  document.querySelector('body > header > ul > li:nth-child(3) > span ').style.color= '';
-  document.querySelector('body > header > ul > li:nth-child(4) > span ').style.color= '';
-  document.querySelector('body > header > ul > li:nth-child(5) > span ').style.color= '';
-  document.querySelector("body > main > section.OurServices").scrollIntoView({ behavior: 'smooth' });
-  }
-  });*/
-  
 
 const handler2 = (event) => {
   if(event.target.classList.contains('web')){
@@ -77,9 +109,40 @@ if(event.target.classList.contains('all')){
   }
   event.target.classList.add('selected2');    
 }
+function anim()
+{
+  document.querySelectorAll("body > div.Slider").forEach(Slider=>Slider.classList.add('to_left'))
+  //document.querySelectorAll("body > div:nth-child(2) > img.iphone1").forEach(iphone1=>iphone1.classList.add('to_left'))
+  //document.querySelectorAll("body > div.Slider > img.gor").forEach(gor=>gor.classList.add('to_left'))
+  //document.querySelectorAll("body > div.Slider > img.iphone2").forEach(iphone2=>iphone2.classList.add('to_left'))
+  //document.querySelectorAll("body > div.Slider > img.vert").forEach(vert=>vert.classList.add('to_left'))
 
-
+  //document.querySelector("body > div.Slider > img.gor").addEventListener('animationend', passiv);
+  //document.querySelectorAll("body > div:nth-child(2) > img.iphone1").forEach(iphone1=>iphone1.classList.add('passiv'))
+  //document.querySelectorAll("body > div.Newslider > img.iphone2").forEach(iphone2=>iphone2.classList.add('passiv'))
+  //document.querySelectorAll("body > div.Newslider > img.gor").forEach(gor=>gor.classList.add('passiv'))
+  //document.querySelectorAll("body > div.Newslider > img.left").forEach(left=>left.classList.add('newleft'))
+  //document.querySelectorAll("body > div.Newslider > img.right").forEach(right=>right.classList.add('newright'))
+  document.querySelectorAll("body > div.Slider > img.iphone3.passiv").forEach(passiv=>passiv.classList.remove('passiv'))
+  document.querySelectorAll("body > div.Slider > img.iphone3_left.passiv").forEach(passiv=>passiv.classList.remove('passiv'))
+  document.querySelectorAll("body > div.Slider > img.iphone3_right.passiv").forEach(passiv=>passiv.classList.remove('passiv'))
+}
+function passiv()
+{
+  document.querySelectorAll("body > div:nth-child(2) > img.iphone1").forEach(iphone1=>iphone1.classList.add('passiv'))
+  document.querySelectorAll("body > div.Newslider > img.iphone2").forEach(iphone2=>iphone2.classList.add('passiv'))
+  document.querySelectorAll("body > div.Newslider > img.gor").forEach(gor=>gor.classList.add('passiv'))
+  document.querySelectorAll("body > div.Newslider > img.vert").forEach(vert=>vert.classList.add('passiv'))
+  document.querySelectorAll("body > div.Slider").forEach(Slider=>Slider.classList.remove('Slider'))
+  document.querySelectorAll("body > div:nth-child(2)").forEach(Slider=>Slider.classList.add('Newslider'))
+  
+}
 document.querySelector("body > header > ul").addEventListener('click', handler1);
 document.querySelector('body > div.Portfolio > ul').addEventListener('click', handler2);
+document.querySelector("body > div.Slider > img.left").addEventListener('click', anim)
+//document.querySelector("body > div.Newslider > img.gor").addEventListener('animationend', passiv)
+//document.querySelector("body > div.Newslider > img.iphone2").addEventListener('animationend', passiv)
+//document.querySelector("body > div.Newslider > img.iphone1.to_left").addEventListener('animationend', passiv)
+
 
 
